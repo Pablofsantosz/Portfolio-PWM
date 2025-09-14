e14147497588a2be0396da2264bb364d94beaf1c
pablo felipe <pabloletiane@gmail.com>
2025-09-14 14:15:41 -0300

    criação de um novo estilo pro componente hero com o Hero.module.css

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import "./index.css";
+//import "./index.css";
 import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
     <>
    <Hero/>
+
   </>
   );
 }
--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
 import Image from 'next/image';
+import styles from './Hero.module.css';
 
-const Hero = () => {
+export default function Hero() {
   return (
-    <div className="main">
-        <Image
+    <div className={styles.hero}>
+      <Image
         src="/images/MinhaFoto.jpg"
         alt="Minha Foto"
-        width={500}
-        height={500}
-        />
-        {/*<img src="/images/MinhaFoto.jpg" alt="Logo" className='minha-foto' /> isso da problema na hora de dar deploy(Vercel) */}
-    
-        <h1 >Pablo Felipe dos Santos</h1>
-        <p>{"Studying a Bachelor's Degree in Computer Science (5th semester) at the Catholic University of Pernambuco (UNICAP). Passionate about software development and quality assurance, I'm always looking to improve my skills and contribute to meaningful projects. I develop using <strong>Python, C#, JavaScript, C, Java,</strong> and use libraries and frameworks like React, Django, and Node.js to build impactful projects."}</p>
-    
-       </div>
-  )
+        width={250}
+        height={250}
+        className={styles.avatar}
+      />
+      <h1 className={styles.name}>Pablo Felipe dos Santos</h1>
+      <p className={styles.description}>
+        Estudante de Ciência da Computação (5º período) na Universidade Católica de Pernambuco (UNICAP). Apaixonado por desenvolvimento de software e qualidade
+      </p>
+    </div>
+  );
 }
-
-export default Hero

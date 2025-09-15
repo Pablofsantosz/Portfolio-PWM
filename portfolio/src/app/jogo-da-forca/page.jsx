72354487b36e96fe150d5f20e746cc40945afbc1
pablo felipe <pabloletiane@gmail.com>
2025-09-15 15:31:21 -0300

    Base da forca

--- portfolio/src/app/jogo-da-forca/page.jsx
+++ portfolio/src/app/jogo-da-forca/page.jsx
@@ -1,9 +1,9 @@
-'use client'; 
-import { useState } from 'react'; 
+'use client';
+import { useState } from 'react';
 import Link from 'next/link';
-import Image from 'next/image'; // Importamos o componente de Imagem
 import styles from './JogoDaForca.module.css';
 
+                      // 1.lista de palavras
 const palavras = [
     "cachorro", "gato", "sol", "lua", "estrela",
     "carro", "floresta", "oceano", "fogo", "montanha",
@@ -15,42 +15,23 @@ const palavras = [
     "saúde", "sabedoria", "natureza", "cultura", "alegria"
   ];
 
+                      // 2. function para retornar uma palavra random 
 function escolherPalavraAleatoria() {
   return palavras[Math.floor(Math.random() * palavras.length)];
 }
 
 export default function JogoDaForca() {
+  // 3. Uso a minha nova função para definir a palavra inicial do jogo.
   const [palavraSecreta, setPalavraSecreta] = useState(escolherPalavraAleatoria());
-  const [letrasTentadas, setLetrasTentadas] = useState([]);
-  
-  
-  const [numeroDeErros, setNumeroDeErros] = useState(0);
 
-
-  
+ 
   return (
     <div className={styles.gameContainer}>
-      <Link href="/" className={styles.backLink}>← Voltar ao Portfólio</Link>
-      
+      <Link href="/">← Voltar</Link>
       <h1>Jogo da Forca</h1>
-      
-     
-      <div className={styles.drawingContainer}>
-        <Image 
-          
-          src={`/forca/forca-${numeroDeErros}.png`} 
-          alt={`Forca com ${numeroDeErros} erros`}
-          width={250}
-          height={300}
-        />
-      </div>
-
-      
+      <p>Adivinhe a palavra:</p>
       <div className={styles.wordContainer}>
-        
-      </div>
-      <div className={styles.keyboard}>
-        
+        {palavraSecreta}
       </div>
     </div>
   );
